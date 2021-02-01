e7f7e5e8a3caf47f2970a1e79b510a4ef1440345
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-01 09:53:14 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-
-
-
+  $(".right").click(function() {
+    
+  });
+  $(".wrong").click(function() {
+  });
+  $("#back").click(function() {
+  });
+  $("#forward").click(function() {
+  });
 });
