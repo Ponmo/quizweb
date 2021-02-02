2178cf0ea1d2c5f5990a731733863f5bf2a107cd
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-02 10:15:48 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
   var q = 1;
   $("td").click(function() {
-    $(".Q" + q).css("border", "none");
     $("this").css("border", "1px solid black");
   });
-  $("#forward").click(function() {
+  //$("#forward").click(function() {
     //q++;
     //$("Qnumber");
     //$(".right").css("background-color", "grey");
     //$(".wrong").css("background-color", "grey");
     //include making back button readable. adding a number and an id. var for(q = 1; q < 4; q++) $("#show" +q).click
-  });
+  //});
 });
