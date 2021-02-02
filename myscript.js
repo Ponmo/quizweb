28aa672a924439130c5cda5670f9eb30e7be06b0
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-02 09:24:55 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var q;
+  
   $(".right").click(function() {
     
   });
@@ -7,6 +9,6 @@ $(document).ready(function() {
   $("#back").click(function() {
   });
   $("#forward").click(function() {
-    //include making back button readable. adding a number and an id.
+    //include making back button readable. adding a number and an id. var for(q = 1; q < 4; q++) $("#show" +q).click
   });
 });
