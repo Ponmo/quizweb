ce9a43e02d91910d1d1906f092985efbeade2b15
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-03 09:36:40 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
   var q = 1;
   var select=1;
-  if(q<9)
-  {
-    $(".table").click(function() {
+  $(".table").click(function() {
+    if(q<9) {
       $("#Q" + q).find("td").click(function() {
         $("#Q" + q).find("td").css("border", "none");
       });
@@ -11,8 +10,8 @@ $(document).ready(function() {
         $(this).css("border", "1px solid black");
         select=2;
       });
-    });
-  }
+    }
+  });
   $("#forward").click(function() {
     if(select == 2){
       $("#Q" + q).hide();
