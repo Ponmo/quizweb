6f1d5b5a71d2a8dd5b9d11a3735d01604eb1b895
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-02 18:34:48 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   var q = 1;
+  var select=false;
   $(jQuery("#Q" + q).find("td")).click(function() {
     $(jQuery("#Q" + q).find("td")).css("border", "none");
   });
   $("td").click(function() {
     $(this).css("border", "1px solid black");
+    select=true;
   });
-  $("#forward").click(function() {
-    q++;
-    $("#Qnumber").html(q + "/9");
-    //$(".right").css("background-color", "grey");
-    //$(".wrong").css("background-color", "grey");
-  });
+  if(select=true) {
+    $("#forward").click(function() {
+      q++;
+      $("#Qnumber").html(q + "/9");
+      //$(".right").css("background-color", "grey");
+      //$(".wrong").css("background-color", "grey");
+      select=false;
+    });
+  };
 });
