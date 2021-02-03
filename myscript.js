c317de6fa6af397e2be00223505fd02ba856078e
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-02 18:45:05 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -12,8 +12,7 @@ $(document).ready(function() {
     if(select == 2){
       q++;
       $("#Qnumber").html(q + "/9");
-      //$(".right").css("background-color", "grey");
-      //$(".wrong").css("background-color", "grey");
+      $("#Q" + q).hide();
       select=1;
     }  
   });
