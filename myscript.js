0189ee0dcca1af89a3997cfcb6b5b806be299e91
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-03 09:34:00 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   var q = 1;
   var select=1;
-  $(".table").click(function() {
-    $("#Q" + q).find("td").click(function() {
-      $("#Q" + q).find("td").css("border", "none");
+  if(q<9)
+  {
+    $(".table").click(function() {
+      $("#Q" + q).find("td").click(function() {
+        $("#Q" + q).find("td").css("border", "none");
+      });
+      $("td").click(function() {
+        $(this).css("border", "1px solid black");
+        select=2;
+      });
     });
-    $("td").click(function() {
-      $(this).css("border", "1px solid black");
-      select=2;
-    });
-  });
+  }
   $("#forward").click(function() {
     if(select == 2){
       $("#Q" + q).hide();
