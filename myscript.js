7d1d89ff732797c19aa141b9f84e268c09302b0e
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-03 22:14:33 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var q = 1;
   $(".table").mouseover(function() {
     $("#Q" + q).find("td").click(function() {
-      $("#Q" + q).find("td").css("border", "4px dotted black");
+      $("#Q" + q).find("td").css("border", "4px dashed black");
     });
     $("td").click(function() {
       if(q!=10) {
